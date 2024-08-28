ab0bb60baaaf96e44a33a7ad033b47f48aa00ee5
OE100 <omer.er10@gmail.com>
2024-08-28 19:45:34 +0300

    removed unused class

--- src/main/java/com/oe/rehooked/item/ReHookedItemProperties.java
+++
@@ -1,8 +0,0 @@
-package com.oe.rehooked.item;
-
-import net.minecraft.client.renderer.item.ItemProperties;
-
-public class ReHookedItemProperties {
-    public static void addCustomItemProperties() {
-    }
-}
