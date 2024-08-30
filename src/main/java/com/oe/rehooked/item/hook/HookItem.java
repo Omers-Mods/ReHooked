cc9ea691e20703f08622554f0ffc7744e69ed1bd
OE100 <omer.er10@gmail.com>
2024-08-31 02:04:53 +0300

    pretty casts and curio utils

--- src/main/java/com/oe/rehooked/item/hook/HookItem.java
+++ src/main/java/com/oe/rehooked/item/hook/HookItem.java
@@ -1,7 +1,6 @@
 package com.oe.rehooked.item.hook;
 
 import com.oe.rehooked.capabilities.hooks.IPlayerHookHandler;
-import com.oe.rehooked.capabilities.hooks.PlayerHookCapabilityProvider;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
@@ -9,11 +8,13 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.util.LazyOptional;
 import org.jetbrains.annotations.Nullable;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
+import java.util.Optional;
 
 public class HookItem extends Item implements ICurioItem {
     public static final String HOOK_TYPE_TAG = "hook_type";
@@ -36,17 +37,17 @@ public class HookItem extends Item implements ICurioItem {
     @Override
     public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack) {
         LivingEntity maybePlayer = slotContext.entity();
-        if (maybePlayer instanceof Player)
-            IPlayerHookHandler.FromPlayer((Player) maybePlayer).ifPresent(handler -> 
-                    handler.owner((Player) maybePlayer).hookType(((HookItem) stack.getItem()).hookType)
+        if (maybePlayer instanceof Player player)
+            IPlayerHookHandler.FromPlayer(player).ifPresent(handler -> 
+                    handler.owner(player).hookType(((HookItem) stack.getItem()).hookType)
             );
     }
 
     @Override
     public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
         LivingEntity maybePlayer = slotContext.entity();
-        if (maybePlayer instanceof Player)
-            IPlayerHookHandler.FromPlayer((Player) maybePlayer).ifPresent(handler -> handler.hookType(""));
+        if (maybePlayer instanceof Player player)
+            IPlayerHookHandler.FromPlayer(player).ifPresent(handler -> handler.hookType(""));
     }
 
     @Override
@@ -55,5 +56,13 @@ public class HookItem extends Item implements ICurioItem {
         pTooltipComponents.add(Component.translatable("tooltip.rehooked:" + ((HookItem) pStack.getItem())
                 .hookType + "_hook.info"));
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+    
+    public Optional<IPlayerHookHandler> getHandler(Player player) {
+        LazyOptional<IPlayerHookHandler> handlerLazy = IPlayerHookHandler.FromPlayer(player);
+        if (handlerLazy.isPresent()) {
+            return handlerLazy.resolve().flatMap(handler -> Optional.of(handler.hookType(hookType)));
+        }
+        return Optional.empty();
     }
 }
