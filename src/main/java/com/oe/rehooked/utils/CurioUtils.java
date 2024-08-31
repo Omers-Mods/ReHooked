b7709ecbcb561fc99041fea8c7f222c9019f5a61
OE100 <omer.er10@gmail.com>
2024-09-01 00:15:11 +0300

    removed old imports

--- src/main/java/com/oe/rehooked/utils/CurioUtils.java
+++ src/main/java/com/oe/rehooked/utils/CurioUtils.java
@@ -1,8 +1,6 @@
 package com.oe.rehooked.utils;
 
-import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.util.LazyOptional;
 import top.theillusivec4.curios.api.CuriosApi;
