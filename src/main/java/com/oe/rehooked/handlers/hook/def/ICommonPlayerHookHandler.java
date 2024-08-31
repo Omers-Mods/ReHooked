9a207603610a8ef6804e84a0e8d77515d230ad4e
OE100 <omer.er10@gmail.com>
2024-08-31 03:46:20 +0300

    sided capability provider

--- src/main/java/com/oe/rehooked/handlers/hook/def/ICommonPlayerHookHandler.java
+++ src/main/java/com/oe/rehooked/handlers/hook/def/ICommonPlayerHookHandler.java
@@ -3,6 +3,8 @@ package com.oe.rehooked.handlers.hook.def;
 import com.oe.rehooked.capabilities.hooks.PlayerHookCapabilityProvider;
 import com.oe.rehooked.data.HookData;
 import com.oe.rehooked.entities.hook.HookEntity;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.util.LazyOptional;
@@ -24,6 +26,11 @@ public interface ICommonPlayerHookHandler {
     boolean shouldMoveThisTick();
     Vec3 getDeltaVThisTick();
     static LazyOptional<ICommonPlayerHookHandler> FromPlayer(Player player) {
-        return player.getCapability(PlayerHookCapabilityProvider.PLAYER_HOOK_HANDLER);
+        if (player instanceof LocalPlayer)
+            return player.getCapability(PlayerHookCapabilityProvider.CLIENT_HOOK_HANDLER);
+        else if (player instanceof ServerPlayer)
+            return player.getCapability(PlayerHookCapabilityProvider.SERVER_HOOK_HANDLER);
+        else
+            return LazyOptional.empty();
     }
 }
