8e24414e9f41c1c736348fdc59c678b78b2d6c31
OE100 <omer.er10@gmail.com>
2024-09-01 00:12:10 +0300

    removed the parent class because it just made things less clear

--- src/main/java/com/oe/rehooked/network/packets/client/CHookCapabilityPacket.java
+++ src/main/java/com/oe/rehooked/network/packets/client/CHookCapabilityPacket.java
@@ -1,19 +1,27 @@
 package com.oe.rehooked.network.packets.client;
 
-import com.oe.rehooked.handlers.hook.client.CPlayerHookHandler;
-import com.oe.rehooked.handlers.hook.def.ICommonPlayerHookHandler;
-import com.oe.rehooked.network.packets.common.HookCapabilityPacket;
+import com.mojang.logging.LogUtils;
+import com.oe.rehooked.handlers.hook.def.IClientPlayerHookHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.FriendlyByteBuf;
-import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.network.NetworkEvent;
+import org.slf4j.Logger;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
-public class CHookCapabilityPacket extends HookCapabilityPacket {
-    public CHookCapabilityPacket(State packetType, int additional) {
-        super(packetType.ordinal(), additional);
+public class CHookCapabilityPacket {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    
+    private final State packetType;
+    private final int id;
+    
+    public CHookCapabilityPacket(State packetType, int id) {
+        this.packetType = packetType;
+        this.id = id;
     }
 
     public CHookCapabilityPacket(State packetType) {
@@ -21,24 +29,38 @@ public class CHookCapabilityPacket extends HookCapabilityPacket {
     }
 
     public CHookCapabilityPacket(FriendlyByteBuf buf) {
-        super(buf);
+        packetType = State.Get(buf.readInt());
+        id = buf.readInt();
     }
 
-    @Override
-    public void handle(Supplier<NetworkEvent.Context> context) {
-        context.get().enqueueWork(() -> {
-            LocalPlayer player = Minecraft.getInstance().player;
-            if (player == null) return;
-            LazyOptional<CPlayerHookHandler> lazyHandler = ICommonPlayerHookHandler.FromPlayer(player)
-                    .lazyMap(handler -> (CPlayerHookHandler) handler);
-            switch (State.Get(packetType)) {
-                case ADD_HOOK -> lazyHandler.ifPresent(handler -> handler.addHook(additional));
-                case RETRACT_HOOK -> lazyHandler.ifPresent(handler -> handler.removeHook(additional));
-                case RETRACT_ALL_HOOKS -> lazyHandler.ifPresent(ICommonPlayerHookHandler::removeAllHooks);
-                case FORCE_UPDATE -> lazyHandler.ifPresent(ICommonPlayerHookHandler::update);
-            }
-        });
+    public void encode(FriendlyByteBuf buf) {
+        buf.writeInt(packetType.ordinal());
+        buf.writeInt(id);
+    }
+
+    public static void handle(CHookCapabilityPacket packet,  Supplier<NetworkEvent.Context> context) {
+        context.get().enqueueWork(() -> 
+                DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> packet::handle));
         context.get().setPacketHandled(true);
+    }
+    
+    public void handle() {
+        LOGGER.debug("Handling client hook packet for: {}", packetType);
+        LocalPlayer player = Minecraft.getInstance().player;
+        if (player == null) return;
+        Optional<IClientPlayerHookHandler> optHandler = IClientPlayerHookHandler.FromPlayer(player).resolve();
+        if (optHandler.isPresent()) {
+            IClientPlayerHookHandler handler = optHandler.get();
+            switch (packetType) {
+                case ADD_HOOK -> handler.addHook(id);
+                case RETRACT_HOOK -> handler.removeHook(id);
+                case RETRACT_ALL_HOOKS -> handler.removeAllHooks();
+                case FORCE_UPDATE -> handler.update();
+            }
+        }
+        else {
+            LOGGER.debug("Missing handler capability");
+        }
     }
     
     public enum State {
--- src/main/java/com/oe/rehooked/network/packets/server/SHookCapabilityPacket.java
+++ src/main/java/com/oe/rehooked/network/packets/server/SHookCapabilityPacket.java
@@ -1,63 +1,72 @@
 package com.oe.rehooked.network.packets.server;
 
 import com.mojang.logging.LogUtils;
-import com.oe.rehooked.handlers.hook.def.ICommonPlayerHookHandler;
-import com.oe.rehooked.handlers.hook.server.SPlayerHookHandler;
-import com.oe.rehooked.network.packets.common.HookCapabilityPacket;
+import com.oe.rehooked.handlers.hook.def.IServerPlayerHookHandler;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.network.NetworkEvent;
 import org.slf4j.Logger;
 
+import java.util.Optional;
 import java.util.function.Supplier;
 
-public class SHookCapabilityPacket extends HookCapabilityPacket {
-    private static Logger LOGGER = LogUtils.getLogger();
+public class SHookCapabilityPacket {
+    private static final Logger LOGGER = LogUtils.getLogger();
     
+    private final State packetType;
+    private final int id;
     private final float xRot;
     private final float yRot;
     
-    public SHookCapabilityPacket(State packetType, int additional, float xRot, float yRot) {
-        super(packetType.ordinal(), additional);
+    public SHookCapabilityPacket(State packetType, int id, float xRot, float yRot) {
+        this.packetType = packetType;
+        this.id = id;
         this.xRot = xRot;
         this.yRot = yRot;
     }
     
-    public SHookCapabilityPacket(State packetType, int additional) {
-        this(packetType, additional, 0, 0);
+    public SHookCapabilityPacket(State packetType, int id) {
+        this(packetType, id, 0, 0);
     }
     
     public SHookCapabilityPacket(State packetType) {
         this(packetType, 0);
     }
-
-    @Override
+    
     public void encode(FriendlyByteBuf buf) {
-        super.encode(buf);
+        buf.writeInt(packetType.ordinal());
+        buf.writeInt(id);
         buf.writeFloat(xRot);
         buf.writeFloat(yRot);
     }
 
-    public SHookCapabilityPacket(FriendlyByteBuf buffer) {
-        super(buffer.readInt(), buffer.readInt());
-        xRot = buffer.readFloat();
-        yRot = buffer.readFloat();
+    public SHookCapabilityPacket(FriendlyByteBuf buf) {
+        packetType = State.Get(buf.readInt());
+        id = buf.readInt();
+        xRot = buf.readFloat();
+        yRot = buf.readFloat();
     }
-
-    @Override
+    
     public void handle(Supplier<NetworkEvent.Context> context) {
         context.get().enqueueWork(() -> {
+            LOGGER.debug("Handling server hook packet for: {}", packetType);
             ServerPlayer player = context.get().getSender();
-            if (player == null || player.level().isClientSide()) return;
-            ICommonPlayerHookHandler.FromPlayer(player).lazyMap(handler -> (SPlayerHookHandler) handler).ifPresent(handler -> {
+            if (player == null) return;
+            Optional<IServerPlayerHookHandler> optHandler = IServerPlayerHookHandler.FromPlayer(player).resolve();
+            if (optHandler.isPresent()) {
+                IServerPlayerHookHandler handler = optHandler.get();
+                LOGGER.debug("Got hook handler");
                 handler.setOwner(player);
-                switch (State.Get(packetType)) {
+                switch (packetType) {
                     case SHOOT -> handler.shootFromRotation(xRot, yRot);
-                    case RETRACT_HOOK -> handler.removeHook(additional);
+                    case RETRACT_HOOK -> handler.removeHook(id);
                     case RETRACT_ALL_HOOKS -> handler.removeAllHooks();
                     case FORCE_UPDATE -> handler.update();
                 }
-            });
+            }
+            else {
+                LOGGER.debug("Missing handler capability");
+            }
         });
         context.get().setPacketHandled(true);
     }
