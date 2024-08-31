3e8cecc2e898ee92a05932ee1c6c4a75f60b3a86
OE100 <omer.er10@gmail.com>
2024-09-01 00:15:46 +0300

    added logs for easier debugs and switch to new api

--- src/main/java/com/oe/rehooked/events/subscribers/client/ClientForgeEvents.java
+++ src/main/java/com/oe/rehooked/events/subscribers/client/ClientForgeEvents.java
@@ -3,7 +3,7 @@ package com.oe.rehooked.events.subscribers.client;
 import com.mojang.logging.LogUtils;
 import com.oe.rehooked.ReHookedMod;
 import com.oe.rehooked.client.KeyBindings;
-import com.oe.rehooked.handlers.hook.def.ICommonPlayerHookHandler;
+import com.oe.rehooked.handlers.hook.def.IClientPlayerHookHandler;
 import com.oe.rehooked.item.hook.HookItem;
 import com.oe.rehooked.network.handlers.PacketHandler;
 import com.oe.rehooked.network.packets.server.SHookCapabilityPacket;
@@ -17,6 +17,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.slf4j.Logger;
 
+import java.util.Optional;
+
 @Mod.EventBusSubscriber(modid = ReHookedMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientForgeEvents {
     private static final Logger LOGGER = LogUtils.getLogger();
@@ -25,23 +27,28 @@ public class ClientForgeEvents {
     
     @SubscribeEvent
     public static void onClientTick(TickEvent.ClientTickEvent event) {
+        if (event.side.isServer()) LOGGER.debug("This event runs on the server too");
         Player player = Minecraft.getInstance().player;
         if (player == null) return;
         ticksSinceShot++;
         if (KeyBindings.FIRE_HOOK_KEY.consumeClick() && ticksSinceShot > 5) {
             ticksSinceShot = 0;
-            LOGGER.debug("Player pressed shoot key");
-            CurioUtils.GetCuriosOfType(HookItem.class, player).flatMap(CurioUtils::GetIfUnique).ifPresent(hookItem -> {
-                Entity camera = Minecraft.getInstance().getCameraEntity();
-                ICommonPlayerHookHandler.FromPlayer(player)
-                        .ifPresent(handler -> handler.shootFromRotation(camera.getXRot(), camera.getYRot()));
+            CurioUtils.GetCuriosOfType(HookItem.class, player).flatMap(CurioUtils::GetIfUnique).ifPresent(hookStack -> {
+                Optional<IClientPlayerHookHandler> optHandler = IClientPlayerHookHandler.FromPlayer(player).resolve();
+                if (optHandler.isPresent()) {
+                    IClientPlayerHookHandler handler = optHandler.get();
+                    Entity camera = Minecraft.getInstance().getCameraEntity();
+                    handler.shootFromRotation(camera.getXRot(), camera.getYRot());
+                } else {
+                    LOGGER.debug("Missing handler capability");
+                }
             });
         }
         if (KeyBindings.REMOVE_ALL_HOOKS_KEY.consumeClick()) {
             LOGGER.debug("Player pressed remove all hooks key");
             CurioUtils.GetCuriosOfType(HookItem.class, player).flatMap(CurioUtils::GetIfUnique).ifPresent(hookItem -> {
                 PacketHandler.sendToServer(new SHookCapabilityPacket(SHookCapabilityPacket.State.RETRACT_ALL_HOOKS));
-                ICommonPlayerHookHandler.FromPlayer(player).ifPresent(ICommonPlayerHookHandler::removeAllHooks);
+                IClientPlayerHookHandler.FromPlayer(player).ifPresent(IClientPlayerHookHandler::removeAllHooks);
             });
         }
     }
@@ -51,7 +58,7 @@ public class ClientForgeEvents {
         Player player = event.player;
         if (player == null) return;
         if (event.phase.equals(TickEvent.Phase.END)) {
-            ICommonPlayerHookHandler.FromPlayer(player).ifPresent(handler -> {
+            IClientPlayerHookHandler.FromPlayer(player).ifPresent(handler -> {
                 handler.update();
                 if (handler.shouldMoveThisTick()) {
                     player.setDeltaMovement(handler.getDeltaVThisTick());
