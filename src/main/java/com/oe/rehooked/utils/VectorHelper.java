52a3ce761eff56a38d60e7657d3076085bab89ea
OE100 <omer.er10@gmail.com>
2024-09-02 13:51:52 +0300

    added utility function to find entities in a cylinder of vision

--- src/main/java/com/oe/rehooked/utils/VectorHelper.java
+++ src/main/java/com/oe/rehooked/utils/VectorHelper.java
@@ -1,25 +1,18 @@
 package com.oe.rehooked.utils;
 
+import com.oe.rehooked.handlers.hook.def.IClientPlayerHookHandler;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.Vec3;
 
-// Shamelessly stolen from https://github.com/Direwolf20-MC/MiningGadgets/blob/mc/1.20.1/src/main/java/com/direwolf20/mininggadgets/common/util/VectorHelper.java, thanks dire!
+import java.util.List;
+import java.util.Optional;
+
 public class VectorHelper {
-    public static BlockHitResult getLookingAt(Entity entity, double range) {
-        return getLookingAt(entity, ClipContext.Fluid.NONE, range);
-    }
-    
-    public static BlockHitResult getLookingAt(Entity entity, ClipContext.Fluid rayTraceFluid, double range) {
-        Level world = entity.level();
-        Vec3 start = entity.getEyePosition();
-        Vec3 end = entity.getEyePosition().add(entity.getLookAngle().scale(range));
-        ClipContext context = new ClipContext(start, end, ClipContext.Block.COLLIDER, rayTraceFluid, entity);
-        return world.clip(context);
-    }
-    
     public static BlockHitResult getFromEntityAndAngle(Entity entity, Vec3 angle, double range) {
         return getFromEntityAndAngle(entity, angle, ClipContext.Fluid.NONE, range);
     }
@@ -29,5 +22,36 @@ public class VectorHelper {
         Vec3 end = entity.getEyePosition().add(angle.scale(range));
         ClipContext context = new ClipContext(entity.getEyePosition(), end, ClipContext.Block.COLLIDER, rayTraceFluid, entity);
         return world.clip(context);
+    }
+    
+    /**
+     * Based on <a href="https://github.com/coolAlias/ZeldaSwordSkills/blob/master/src/main/java/zeldaswordskills/util/TargetUtils.java">ZeldaSwordSkills</a>
+     * <p> 
+     * This method is client-side only!
+     */
+    public static <T extends Entity> Optional<T> acquireLookTarget(Class<T> clazz, Player seeker, double radius) {
+        Vec3 lookAngle = seeker.getLookAngle();
+        double targetX = seeker.getX();
+        double targetY = seeker.getEyeY();
+        double targetZ = seeker.getZ();
+        double distanceTraveled = 0;
+
+        Optional<IClientPlayerHookHandler> optHandler = IClientPlayerHookHandler.FromPlayer(seeker).resolve();
+        if (optHandler.isPresent()) {
+            IClientPlayerHookHandler handler = optHandler.get();
+            double range = handler.getMaxHookDistance();
+
+            while ((int) distanceTraveled < range) {
+                targetX += lookAngle.x;
+                targetY += lookAngle.y;
+                targetZ += lookAngle.z;
+                distanceTraveled += 1;
+                AABB bb = new AABB(targetX-radius, targetY-radius, targetZ-radius, targetX+radius, targetY+radius, targetZ+radius);
+                List<T> list = seeker.level().getEntitiesOfClass(clazz, bb);
+                for (T target : list) if (target != seeker) return Optional.of(target);
+            }
+        }
+        
+        return Optional.empty();
     }
 }
