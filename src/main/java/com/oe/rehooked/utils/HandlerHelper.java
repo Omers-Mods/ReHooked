fea5a9db3bcedfc8902cdfbe5bce1897ecdd9cb2
OE100 <omer.er10@gmail.com>
2024-09-04 01:46:47 +0300

    utility function to get the hook handler

---
+++ src/main/java/com/oe/rehooked/utils/HandlerHelper.java
@@ -0,0 +1,16 @@
+package com.oe.rehooked.utils;
+
+import com.oe.rehooked.handlers.hook.def.IClientPlayerHookHandler;
+import com.oe.rehooked.handlers.hook.def.ICommonPlayerHookHandler;
+import com.oe.rehooked.handlers.hook.def.IServerPlayerHookHandler;
+import net.minecraft.world.entity.player.Player;
+
+import java.util.Optional;
+
+public class HandlerHelper {
+    public static Optional<? extends ICommonPlayerHookHandler> getHookHandler(Player player) {
+        if (player == null) return Optional.empty();
+        if (player.level().isClientSide()) return IClientPlayerHookHandler.FromPlayer(player).resolve();
+        return IServerPlayerHookHandler.FromPlayer(player).resolve();
+    }
+}
