15b78cbfba695a6e9aa84a33ad05bf9c5ec9c0c1
OE100 <omer.er10@gmail.com>
2024-09-04 02:28:22 +0300

    added call to handler on equip and unequip

--- src/main/java/com/oe/rehooked/item/hook/HookItem.java
+++ src/main/java/com/oe/rehooked/item/hook/HookItem.java
@@ -1,11 +1,15 @@
 package com.oe.rehooked.item.hook;
 
+import com.oe.rehooked.handlers.hook.def.ICommonPlayerHookHandler;
+import com.oe.rehooked.utils.HandlerHelper;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
@@ -32,5 +36,19 @@ public class HookItem extends Item implements ICurioItem {
         pTooltipComponents.add(Component.translatable("tooltip.rehooked:" + ((HookItem) pStack.getItem())
                 .hookType + "_hook.info"));
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
+    @Override
+    public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
+        ICurioItem.super.onUnequip(slotContext, newStack, stack);
+        if (!(slotContext.entity() instanceof Player owner)) return;
+        HandlerHelper.getHookHandler(owner).ifPresent(ICommonPlayerHookHandler::onUnequip);
+    }
+
+    @Override
+    public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack) {
+        ICurioItem.super.onEquip(slotContext, prevStack, stack);
+        if (!(slotContext.entity() instanceof Player owner)) return;
+        HandlerHelper.getHookHandler(owner).ifPresent(ICommonPlayerHookHandler::onEquip);
     }
 }
