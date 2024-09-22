efc86805dd49b2b6c50152c2cd52237108f02aa5
OE100 <omer.er10@gmail.com>
2024-09-23 02:16:05 +0300

    removed unused import

--- src/main/java/com/oe/rehooked/data/HookData.java
+++ src/main/java/com/oe/rehooked/data/HookData.java
@@ -3,7 +3,6 @@ package com.oe.rehooked.data;
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.resources.ResourceLocation;
 
-import java.util.Optional;
 import java.util.function.Supplier;
 
 public record HookData(int count, float range, float speed, float pullSpeed, boolean isCreative, ResourceLocation texture, Supplier<ParticleOptions> particleType) {
