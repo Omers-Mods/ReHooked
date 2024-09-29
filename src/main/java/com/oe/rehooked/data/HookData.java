9c67fd4f6c53813e1cb35b653a8f77ece3f50eab
OE100 <omer.er10@gmail.com>
2024-09-29 23:24:38 +0300

    updated hook data for easier config of particles

--- src/main/java/com/oe/rehooked/data/HookData.java
+++ src/main/java/com/oe/rehooked/data/HookData.java
@@ -5,5 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 
 import java.util.function.Supplier;
 
-public record HookData(int count, float range, float speed, float pullSpeed, boolean isCreative, ResourceLocation texture, Supplier<ParticleOptions> particleType) {
+public record HookData(String type, int count, float range, float speed, float pullSpeed, boolean isCreative, 
+                       ResourceLocation texture, 
+                       Supplier<ParticleOptions> particleType, int minParticlesPerBlock, int maxParticlesPerBlock, double radius, int ticksBetweenSpawns) {
 }
