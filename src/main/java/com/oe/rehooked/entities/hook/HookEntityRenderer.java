e8c9052bee3d99809478e05ec8825fe93fc4635f
OE100 <omer.er10@gmail.com>
2024-10-16 05:06:27 +0300

    massive render change, was a pain in my ass

--- src/main/java/com/oe/rehooked/entities/hook/HookEntityRenderer.java
+++ src/main/java/com/oe/rehooked/entities/hook/HookEntityRenderer.java
@@ -2,6 +2,7 @@ package com.oe.rehooked.entities.hook;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
+import com.oe.rehooked.data.ChainRegistry;
 import com.oe.rehooked.data.HookData;
 import com.oe.rehooked.entities.layers.ReHookedModelLayers;
 import com.oe.rehooked.utils.PositionHelper;
@@ -17,8 +18,8 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 
 public class HookEntityRenderer extends EntityRenderer<HookEntity> {
@@ -54,18 +55,18 @@ public class HookEntityRenderer extends EntityRenderer<HookEntity> {
 
     private void handleHook(HookEntity pEntity, float pEntityYaw, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
         pPoseStack.pushPose();
-        Vec3 dV = pEntity.getDeltaMovement();
-        if (!dV.equals(Vec3.ZERO)) {
-            pEntity.lookAt(EntityAnchorArgument.Anchor.EYES, pEntity.getEyePosition().add(dV));
-        }
-        else {
-            pEntity.getHitPos()
-                    .ifPresent(blockPos -> pEntity.lookAt(EntityAnchorArgument.Anchor.EYES, blockPos.getCenter()));
-        }
-        pPoseStack.mulPose(Axis.YP.rotationDegrees(270f - pEntity.getYRot()));
-        pPoseStack.mulPose(Axis.ZP.rotationDegrees(90f - pEntity.getXRot()));
-        pPoseStack.scale(0.4f, 0.4f, 0.4f);
+        Vec3 direction = new Vec3(pEntity.getShotDirection());
+        Vec3 lookAt = pEntity.getEyePosition().add(direction);
+        pEntity.lookAt(EntityAnchorArgument.Anchor.EYES, lookAt);
+        AABB box = pEntity.getBoundingBox();
+        pPoseStack.scale((float) box.getXsize(), (float) box.getYsize(), (float) box.getZsize());
+        pPoseStack.mulPose(Axis.ZP.rotationDegrees(180f));
+        pPoseStack.mulPose(Axis.YP.rotationDegrees(pEntity.getYRot() + 270f));
+        pPoseStack.mulPose(Axis.ZP.rotationDegrees(pEntity.getXRot() + 90f));
         pPoseStack.translate(0, -1, 0);
+        if (pEntity.isInWall()) {
+            pPoseStack.translate(0, direction.y - 1, 0);
+        }
         this.model.renderToBuffer(pPoseStack, pBuffer.getBuffer(model.renderType(getTextureLocation(pEntity))), pPackedLight, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
         pPoseStack.popPose();
     }
@@ -84,20 +85,18 @@ public class HookEntityRenderer extends EntityRenderer<HookEntity> {
         pPoseStack.translate(-0.5, 0.0, -0.5);
         // add chains
         float distance = (float) playerToHook.length();
-        pEntity.getHookData().ifPresent(hookData -> {
-            BlockState chain = Blocks.CHAIN.defaultBlockState();
-            for (int i = 0; i < (int) distance; i++) {
-                pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, pPackedLight, OverlayTexture.NO_OVERLAY);
-                pPoseStack.translate(0, 1, 0);
-            }
-            float delta = distance - (int) distance;
-            if (lastDelta == 0) lastDelta = delta;
-            if (Math.abs(delta - lastDelta) < 0.3f) delta = lastDelta;
-            pPoseStack.scale(1, delta, 1);
-            lastDelta = delta;
+        BlockState chain = ChainRegistry.getChain(pEntity.getHookType()).defaultBlockState();
+        for (int i = 0; i < (int) distance; i++) {
             pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, pPackedLight, OverlayTexture.NO_OVERLAY);
-            pPoseStack.popPose();
-        });
+            pPoseStack.translate(0, 1, 0);
+        }
+        float delta = distance - (int) distance;
+        if (lastDelta == 0) lastDelta = delta;
+        if (Math.abs(delta - lastDelta) < 0.3f) delta = lastDelta;
+        pPoseStack.scale(1, delta, 1);
+        lastDelta = delta;
+        pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, pPackedLight, OverlayTexture.NO_OVERLAY);
+        pPoseStack.popPose();
     }
     
     @Override
