d95b52dc1a6ebd04c11ed9eb2024ae39828647ce
OE100 <omer.er10@gmail.com>
2024-10-16 05:06:07 +0300

    blaze hook name convention change

--- src/main/java/com/oe/rehooked/datagen/ReHookedItemTagProvider.java
+++ src/main/java/com/oe/rehooked/datagen/ReHookedItemTagProvider.java
@@ -6,7 +6,6 @@ import com.oe.rehooked.tags.ReHookedTags;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -26,26 +25,8 @@ public class ReHookedItemTagProvider extends ItemTagsProvider {
                 .add(ReHookedItems.IRON_HOOK.get())
                 .add(ReHookedItems.DIAMOND_HOOK.get())
                 .add(ReHookedItems.RED_HOOK.get())
-                .add(ReHookedItems.BLAZING_HOOK.get())
+                .add(ReHookedItems.BLAZE_HOOK.get())
                 .add(ReHookedItems.ENDER_HOOK.get());
-        
-        this.tag(ReHookedTags.Items.MUSIC_DISC)
-                .add(Items.MUSIC_DISC_13)
-                .add(Items.MUSIC_DISC_CAT)
-                .add(Items.MUSIC_DISC_BLOCKS)
-                .add(Items.MUSIC_DISC_CHIRP)
-                .add(Items.MUSIC_DISC_FAR)
-                .add(Items.MUSIC_DISC_MALL)
-                .add(Items.MUSIC_DISC_MELLOHI)
-                .add(Items.MUSIC_DISC_STAL)
-                .add(Items.MUSIC_DISC_STRAD)
-                .add(Items.MUSIC_DISC_WARD)
-                .add(Items.MUSIC_DISC_11)
-                .add(Items.MUSIC_DISC_WAIT)
-                .add(Items.MUSIC_DISC_PIGSTEP)
-                .add(Items.MUSIC_DISC_OTHERSIDE)
-                .add(Items.MUSIC_DISC_RELIC)
-                .add(Items.MUSIC_DISC_5);
     }
 
     @Override
