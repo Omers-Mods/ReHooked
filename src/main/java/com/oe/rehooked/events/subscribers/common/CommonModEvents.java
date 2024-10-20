1cd295c7dc351e791f2dae46b996abbcbb2ba951
OE100 <omer.er10@gmail.com>
2024-10-20 04:05:22 +0300

    removed unused import

--- src/main/java/com/oe/rehooked/events/subscribers/common/CommonModEvents.java
+++ src/main/java/com/oe/rehooked/events/subscribers/common/CommonModEvents.java
@@ -4,7 +4,6 @@ import com.oe.rehooked.ReHookedMod;
 import com.oe.rehooked.item.ReHookedItems;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = ReHookedMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
