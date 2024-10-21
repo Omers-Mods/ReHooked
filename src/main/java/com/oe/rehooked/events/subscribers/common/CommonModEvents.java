0bb100919f2b404a1adc9f2ad54629e342f9b134
OE100 <omer.er10@gmail.com>
2024-10-21 22:55:18 +0300

    changed loading of config entries only to happen only when server config reloads

--- src/main/java/com/oe/rehooked/events/subscribers/common/CommonModEvents.java
+++ src/main/java/com/oe/rehooked/events/subscribers/common/CommonModEvents.java
@@ -4,23 +4,24 @@ import com.oe.rehooked.ReHookedMod;
 import com.oe.rehooked.item.ReHookedItems;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.config.ModConfigEvent;
 
 @Mod.EventBusSubscriber(modid = ReHookedMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CommonModEvents {
     @SubscribeEvent
     public static void onLoadConfig(ModConfigEvent.Loading event) {
-        loadConfig();
+        if (event.getConfig().getType().equals(ModConfig.Type.SERVER))
+            loadConfig();
     }
     
     @SubscribeEvent
     public static void onReloadConfig(ModConfigEvent.Reloading event) {
-        loadConfig();
+        if (event.getConfig().getType().equals(ModConfig.Type.SERVER))
+            loadConfig();
     }
     
     private static void loadConfig() {
-        ReHookedMod.LOGGER.info("Server config loaded, registering item properties.");
         ReHookedItems.RegisterConfigProperties();
-        ReHookedMod.LOGGER.info("Finished registering item properties from config.");
     }
 }
