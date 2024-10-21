b6aafce46ad2e9b5d384f7ff03e1213cfb5e0231
OE100 <omer.er10@gmail.com>
2024-10-21 22:52:14 +0300

    removed custom chain classes, not needed

--- src/main/java/com/oe/rehooked/item/component/BlazeChain.java
+++
@@ -1,9 +0,0 @@
-package com.oe.rehooked.item.component;
-
-import net.minecraft.world.item.Item;
-
-public class BlazeChain extends Item {
-    public BlazeChain() {
-        super(new Properties().defaultDurability(0).stacksTo(16));
-    }
-}
--- src/main/java/com/oe/rehooked/item/component/EnderChain.java
+++
@@ -1,9 +0,0 @@
-package com.oe.rehooked.item.component;
-
-import net.minecraft.world.item.Item;
-
-public class EnderChain extends Item {
-    public EnderChain() {
-        super(new Properties().defaultDurability(0).stacksTo(16));
-    }
-}
