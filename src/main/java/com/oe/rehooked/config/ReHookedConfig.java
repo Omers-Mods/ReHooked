485ef655dabd28e81b919630bbc258931629b532
OE100 <omer.er10@gmail.com>
2024-10-21 22:52:53 +0300

    added new client config

--- src/main/java/com/oe/rehooked/config/ReHookedConfig.java
+++ src/main/java/com/oe/rehooked/config/ReHookedConfig.java
@@ -1,5 +1,6 @@
 package com.oe.rehooked.config;
 
+import com.oe.rehooked.config.client.visuals.HookVisualsConfig;
 import com.oe.rehooked.config.server.stats.HookStatsConfig;
 import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -7,16 +8,22 @@ import net.minecraftforge.fml.config.ModConfig;
 
 public class ReHookedConfig {
     private static final ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
+    private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
     
     static {
         createConfig();
     }
     
     private static void createConfig() {
+        // server config entries
         HookStatsConfig.Init(SERVER_BUILDER);
+        
+        // client config entries
+        HookVisualsConfig.Init(CLIENT_BUILDER);
     }
     
     public static void Init() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_BUILDER.build());
+        ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CLIENT_BUILDER.build());
     }
 }
