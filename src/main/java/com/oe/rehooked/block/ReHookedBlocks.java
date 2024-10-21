7dddf0d43b1895f44b2a10916f68b5513290a40d
OE100 <omer.er10@gmail.com>
2024-10-22 02:10:13 +0300

    chain stuff

--- src/main/java/com/oe/rehooked/block/ReHookedBlocks.java
+++ src/main/java/com/oe/rehooked/block/ReHookedBlocks.java
@@ -4,8 +4,8 @@ import com.oe.rehooked.ReHookedMod;
 import com.oe.rehooked.data.ChainRegistry;
 import com.oe.rehooked.item.ReHookedItems;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.ChainBlock;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,11 +14,12 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class ReHookedBlocks {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ReHookedMod.MOD_ID);
-
-    public static final RegistryObject<Block> WOOD_CHAIN = BLOCKS.register("wood_chain", () -> new ChainBlock(BlockBehaviour.Properties.copy(Blocks.CHAIN)));
-    public static final RegistryObject<Block> DIAMOND_CHAIN = BLOCKS.register("diamond_chain", () -> new ChainBlock(BlockBehaviour.Properties.copy(Blocks.CHAIN)));
-    public static final RegistryObject<Block> BLAZE_CHAIN = BLOCKS.register("blaze_chain", () -> new ChainBlock(BlockBehaviour.Properties.copy(Blocks.CHAIN)));
-    public static final RegistryObject<Block> ENDER_CHAIN = BLOCKS.register("ender_chain", () -> new ChainBlock(BlockBehaviour.Properties.copy(Blocks.CHAIN)));
+    
+    public static final BlockBehaviour.Properties CHAIN_PROPS = BlockBehaviour.Properties.of().forceSolidOn().requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.CHAIN).noOcclusion();
+    public static final RegistryObject<Block> WOOD_CHAIN = BLOCKS.register("wood_chain", () -> new ChainBlock(CHAIN_PROPS));
+    public static final RegistryObject<Block> DIAMOND_CHAIN = BLOCKS.register("diamond_chain", () -> new ChainBlock(CHAIN_PROPS));
+    public static final RegistryObject<Block> BLAZE_CHAIN = BLOCKS.register("blaze_chain", () -> new ChainBlock(CHAIN_PROPS));
+    public static final RegistryObject<Block> ENDER_CHAIN = BLOCKS.register("ender_chain", () -> new ChainBlock(CHAIN_PROPS));
     
     public static void Init(IEventBus eventBus) {
         BLOCKS.register(eventBus);
