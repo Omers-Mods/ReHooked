abea79e8002b3d0c50f7b0b93a0b45b31f28a1a6
OE100 <omer.er10@gmail.com>
2024-10-21 10:25:52 +0300

    added blaze chain added ender chain item

---
+++ src/main/java/com/oe/rehooked/item/component/BlazeChain.java
@@ -0,0 +1,9 @@
+package com.oe.rehooked.item.component;
+
+import net.minecraft.world.item.Item;
+
+public class BlazeChain extends Item {
+    public BlazeChain() {
+        super(new Properties().defaultDurability(0).stacksTo(16));
+    }
+}
---
+++ src/main/java/com/oe/rehooked/item/component/EnderChain.java
@@ -0,0 +1,9 @@
+package com.oe.rehooked.item.component;
+
+import net.minecraft.world.item.Item;
+
+public class EnderChain extends Item {
+    public EnderChain() {
+        super(new Properties().defaultDurability(0).stacksTo(16));
+    }
+}
