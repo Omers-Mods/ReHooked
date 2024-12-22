22a58f3a27c2e117c0527bc3953fd05d7b3d86b2
omergunr100 <omer.er10@gmail.com>
2024-12-22 16:11:38 +0200

    dynamic lighting for hooks

--- src/main/java/com/oe/rehooked/entities/hook/HookEntityRenderer.java
+++ src/main/java/com/oe/rehooked/entities/hook/HookEntityRenderer.java
@@ -8,16 +8,20 @@ import com.oe.rehooked.entities.layers.ReHookedModelLayers;
 import com.oe.rehooked.utils.PositionHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.commands.arguments.EntityAnchorArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.LightLayer;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -48,9 +52,21 @@ public class HookEntityRenderer extends EntityRenderer<HookEntity> {
 
     @Override
     public void render(HookEntity pEntity, float pEntityYaw, float pPartialTicks, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
-        handleHook(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, 15728880);
-        if (pEntity.hasChain() && pEntity.tryGetOwnerFromCachedId() != null) handleChain(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, pPackedLight);
-        super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, 15728880);
+        var maxBlockLight = LightTexture.block(pPackedLight);
+        var skyLight = LightTexture.sky(pPackedLight);
+        var pos = pEntity.getLightProbePosition(pPartialTicks);
+        var blockPos = BlockPos.containing(pos);
+        for (var direction : Direction.values()) {
+            var blockLight = pEntity.level().getBrightness(LightLayer.BLOCK, blockPos.relative(direction));
+            if (blockLight > maxBlockLight) {
+                maxBlockLight = blockLight;
+            }
+        }
+        if (maxBlockLight == 0) maxBlockLight++;
+        final var light = LightTexture.pack(maxBlockLight, skyLight);
+        handleHook(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, light);
+        if (pEntity.hasChain() && pEntity.tryGetOwnerFromCachedId() != null) handleChain(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, light);
+        super.render(pEntity, pEntityYaw, pPartialTicks, pPoseStack, pBuffer, light);
     }
 
     private void handleHook(HookEntity pEntity, float pEntityYaw, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight) {
@@ -76,6 +92,7 @@ public class HookEntityRenderer extends EntityRenderer<HookEntity> {
         Vec3 hookPos = pEntity.position();
         Vec3 playerToHook = waistPos.vectorTo(hookPos);
         Vec3 normal = playerToHook.normalize();
+        final var reverseNorm = normal.reverse();
         // rotate
         pPoseStack.mulPose(Axis.YP.rotation(Mth.HALF_PI - (float) Mth.atan2(normal.z, normal.x)));
         pPoseStack.mulPose(Axis.XP.rotation((float) Math.acos(normal.y) - Mth.PI));
@@ -83,8 +100,15 @@ public class HookEntityRenderer extends EntityRenderer<HookEntity> {
         // add chains
         float distance = (float) playerToHook.length();
         BlockState chain = ChainRegistry.getChain(pEntity.getHookType()).defaultBlockState();
+        var lightProbePosition = pEntity.getLightProbePosition(pPartialTick).subtract(reverseNorm);
+        var prevNotBlank = 1;
         for (int i = 0; i < (int) distance; i++) {
-            pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, pPackedLight, OverlayTexture.NO_OVERLAY);
+            lightProbePosition = lightProbePosition.add(reverseNorm);
+            var packedLight = getPackedLight(pEntity, lightProbePosition);
+            var blockLight = LightTexture.block(packedLight);
+            if (blockLight == 1) packedLight = LightTexture.pack(prevNotBlank, LightTexture.sky(packedLight));
+            else prevNotBlank = blockLight;
+            pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, packedLight, OverlayTexture.NO_OVERLAY);
             pPoseStack.translate(0, 1, 0);
         }
         float delta = distance - (int) distance;
@@ -92,10 +116,23 @@ public class HookEntityRenderer extends EntityRenderer<HookEntity> {
         if (Math.abs(delta - lastDelta) < 0.3f) delta = lastDelta;
         pPoseStack.scale(1, delta, 1);
         lastDelta = delta;
-        pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, pPackedLight, OverlayTexture.NO_OVERLAY);
+        lightProbePosition = lightProbePosition.add(reverseNorm.scale(delta));
+        var packedLight = getPackedLight(pEntity, lightProbePosition);
+        var blockLight = LightTexture.block(packedLight);
+        if (blockLight == 1) packedLight = LightTexture.pack(prevNotBlank, LightTexture.sky(packedLight));
+        pContext.getBlockRenderDispatcher().renderSingleBlock(chain, pPoseStack, pBuffer, packedLight, OverlayTexture.NO_OVERLAY);
         pPoseStack.popPose();
     }
     
+    private static int getPackedLight(HookEntity pEntity, Vec3 lightProbePosition) {
+        var blockLightPos = BlockPos.containing(lightProbePosition);
+        var blockLight = pEntity.level().getBrightness(LightLayer.BLOCK, blockLightPos);
+        var skyLight = pEntity.level().getBrightness(LightLayer.SKY, blockLightPos);
+        if (blockLight == 0) blockLight++;
+        if (skyLight == 0) skyLight++;
+        return LightTexture.pack(blockLight, skyLight);
+    }
+
     @Override
     public ResourceLocation getTextureLocation(HookEntity pEntity) {
         return pEntity.getHookData().map(HookData::texture).orElseGet(null);
