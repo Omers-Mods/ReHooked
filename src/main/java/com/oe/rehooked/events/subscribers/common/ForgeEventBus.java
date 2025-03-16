e6d285c19dc938c12f34aab7e50f6af3931ad50e
omergunr100 <omer.er10@gmail.com>
2025-03-16 22:14:45 +0200

    fix crash on dedicated servers

--- src/main/java/com/oe/rehooked/events/subscribers/common/ForgeEventBus.java
+++ src/main/java/com/oe/rehooked/events/subscribers/common/ForgeEventBus.java
@@ -2,6 +2,7 @@ package com.oe.rehooked.events.subscribers.common;
 
 import com.mojang.logging.LogUtils;
 import com.oe.rehooked.ReHookedMod;
+import com.oe.rehooked.entities.hook.HookEntity;
 import com.oe.rehooked.extensions.player.IReHookedPlayerExtension;
 import com.oe.rehooked.handlers.hook.def.IServerPlayerHookHandler;
 import com.oe.rehooked.handlers.hook.server.SPlayerHookHandler;
@@ -64,9 +65,11 @@ public class ForgeEventBus {
     
     @SubscribeEvent
     public static void onDimensionChange(PlayerEvent.PlayerChangedDimensionEvent event) {
-        if (event.getEntity().level().isClientSide()) return;
         HandlerHelper.getHookHandler(event.getEntity()).ifPresent(handler -> {
-            handler.removeAllHooks();
+            if (!event.getEntity().level().isClientSide()) {
+                handler.getHooks().forEach(HookEntity::discard);
+            }
+            handler.getHooks().clear();
             handler.afterDeath();
         });
     }
--- src/main/java/com/oe/rehooked/network/handlers/client/CHookPayloadHandler.java
+++ src/main/java/com/oe/rehooked/network/handlers/client/CHookPayloadHandler.java
@@ -3,30 +3,29 @@ package com.oe.rehooked.network.handlers.client;
 import com.oe.rehooked.handlers.hook.def.IClientPlayerHookHandler;
 import com.oe.rehooked.network.payloads.client.CHookPayload;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 
-import java.util.Optional;
-
 public class CHookPayloadHandler {
     public static void handle(final CHookPayload payload, final IPayloadContext context) {
-        context.enqueueWork(() -> {
-            LocalPlayer player = Minecraft.getInstance().player;
+        context.enqueueWork(() -> Wrapper.handle(payload)).exceptionally(e -> {
+            context.disconnect(Component.literal(e.getMessage()));
+            return null;
+        });
+    }
+    
+    private static class Wrapper {
+        public static void handle(final CHookPayload payload) {
+            var player = Minecraft.getInstance().player;
             if (player == null) return;
-            Optional<IClientPlayerHookHandler> optHandler = IClientPlayerHookHandler.fromPlayer(player);
-            if (optHandler.isPresent()) {
-                IClientPlayerHookHandler handler = optHandler.get();
+            IClientPlayerHookHandler.fromPlayer(player).ifPresent(handler -> {
                 switch (CHookPayload.State.get(payload.state())) {
                     case ADD_HOOK -> handler.addHook(payload.id());
                     case RETRACT_HOOK -> handler.removeHook(payload.id());
                     case RETRACT_ALL_HOOKS -> handler.removeAllHooks();
                     case FORCE_UPDATE -> handler.update();
                 }
-            }
-        }).exceptionally(e -> {
-            context.disconnect(Component.literal(e.getMessage()));
-            return null;
-        });
+            });
+        }
     }
 }
